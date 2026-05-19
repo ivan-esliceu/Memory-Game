a909a53c33c7ce0174e038b05991f7489e8e5b60
ivan-esliceu <itaulerh@esliceu.net>
2026-05-19 20:07:06 +0200

    treure coordenades darrers dos clicks

--- Memory_base.js
+++ Memory_base.js
@@ -14,17 +14,44 @@ for (let i = 0; i < casillas.length; i++) {
 }
 lista_palabras= ["perro","gato","casa","arbol"]
 
-function distribucionaleatoria(lista) {
+function distribucionaleatoriaacoordenadas(lista) {
     lista_duplicada = lista.concat(lista);
- 
+    lista_nueva= [];
     for (let i = 0; i < casillas.length; i++) {
         let elementoRandom = lista_duplicada[Math.floor(Math.random() * lista_duplicada.length)]
-        let casilla = document.getElementById(casillas[i]);
-        casilla.textContent = elementoRandom;
+        lista_nueva.push(elementoRandom);
+        console.log("ESTA NIGGA"+lista_nueva);
         lista_duplicada.splice(lista_duplicada.indexOf(elementoRandom), 1);
 
         console.log(lista_duplicada);
 }
+    return lista_nueva;
+}
+distribucionaleatoriaacoordenadas(lista_palabras);
+
+function sacarcoordendadasultimosdosclicks(){
+    ultimosdosclicks = [];
+    for (let i = 0; i < casillas.length; i++) {
+        let casilla = document.getElementById(casillas[i]);
+        casilla.addEventListener("click", function() {
+            casilla.textContent = "o";
+            console.log(i+1)
+            ultimosdosclicks.push(i+1);
+                if (ultimosdosclicks.length > 2) {
+                    ultimosdosclicks.shift();
+                }
+            console.log(ultimosdosclicks);
+        })
+
 
 }
-distribucionaleatoria(lista_palabras);
+}
+
+sacarcoordendadasultimosdosclicks();
+function parejasposibles(){
+
+}
+
+function revertirpareja(pareja){
+    return [pareja[1], pareja[0]];
+}
