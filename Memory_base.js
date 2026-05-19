477a455d60bb041f3c23f5a2f75ab0f03afaafd2
ivan-esliceu <itaulerh@esliceu.net>
2026-05-19 17:24:54 +0200

    Creacio del layout

--- Memory_base.js
+++ Memory_base.js
@@ -1,6 +1,14 @@
-casillas = [1,2,3,4,5,6,7,8]
+casillas = ["1","2","3","4","5","6","7","8"]
 
 //Función para aleatorizar el orden de las casillas
 
 
-//Función para 
+//Función para clicks
+
+//funcion 
+for (let i = 0; i < casillas.length; i++) {
+    let casilla = document.getElementById(casillas[i]);
+    casilla.addEventListener("click", function() {
+        casilla.textContent = "o";
+    });
+}
